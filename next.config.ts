b1ef70c08e8eb1f72f7cfdafc09ccd23c2753a99
Tori <74414720+toricodesthings@users.noreply.github.com>
2025-07-27 00:58:28 -0400

    Fix build issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
